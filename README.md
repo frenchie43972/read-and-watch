c971fc5fccfd17b00ac670cb2e26e2de2ed5a07f
Kris French <frenchie43972@gmail.com>
2024-03-28 14:32:22 +0900

    Working Navbar component

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 A simple site to share what you are currently reading or watching
 
 Could possibly turn into a quasi-social site. 
+
+
+Possible components needed for each page (already have a navbar):
+  - Right sidebar 
+  - Left sidebar
+  - Footer
+  - Card for both categories
--- frontend/read-and-watch/src/app/components/navbar.tsx
+++ frontend/read-and-watch/src/app/components/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { PiBookBookmarkLight, PiTelevision } from "react-icons/pi";
 
 // USE THIS LATER IF NECESSARY. REFER TO BF CLONE FOR THE REST OF THE CODE
 // type NavLink = {
@@ -13,24 +14,24 @@ import Link from "next/link";
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <div>
-        <div>
-          <Link href={'/'}>R|W</Link>
-        </div>
-        <div>
-          <Link href={'/pages/read'}>
-            Read
-          </Link>
-        </div>
-        <div>
-          <Link href={'/pages/watch'}>
-            Watch
-          </Link>
-        </div>
+    <nav className="flex items-center justify-between p-4 mb-6">
+      <div className="flex-grow justify-center w-1/5 h-12 text-2xl p-1">
+        <Link href={'/'}>R|W</Link>
+      </div>
+      <div className="flex flex-grow justify-center items-center space-x-20 w-3/5 h-12">
+        <Link href={'/pages/read'}>
+          <PiBookBookmarkLight className="size-8" />
+        </Link>
+        <Link href={'/pages/watch'}>
+          <PiTelevision className="size-9" />
+        </Link>
+      </div>
+      <div className="flex-grow w-1/5 h-12">
+        <input className="mt-4" />
       </div>
     </nav>
   );
 };
 
 export default Navbar;
+
